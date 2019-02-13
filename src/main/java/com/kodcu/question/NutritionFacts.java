302ef94e0d27cd6039eac11ffc3f3d406515abc3
altuga@gmail.com <upux1234>
2019-02-13 14:24:00 +0300

    question 1

---
+++ src/main/java/com/kodcu/question/NutritionFacts.java
@@ -0,0 +1,18 @@
+package com.kodcu.question;
+
+// Builder Pattern  (Page 13)
+// TODO initialize the class
+public class NutritionFacts {
+    private final int servingSize = 0;
+    private final int servings = 0 ;
+    private final int calories = 0; // optional
+    private final int fat = 0;  // optional
+    private final int sodium = 0; // optional
+    private final int carbohydrate = 0; // optional
+
+
+    public static void main(String[] args) {
+        NutritionFacts nutritionFacts = new NutritionFacts();
+        System.out.println(nutritionFacts);
+    }
+}
