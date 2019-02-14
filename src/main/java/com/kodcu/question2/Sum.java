fbfc50edb6d70e3fc91c320ccb76d905e58421d6
altuga@gmail.com <upux1234>
2019-02-14 14:47:54 +0300

    question 2

---
+++ src/main/java/com/kodcu/question2/Sum.java
@@ -0,0 +1,21 @@
+package com.kodcu.question2;
+
+public class Sum {
+    private static long sum() {
+        Long sum = 0L;
+        for (long i = 0; i <= Integer.MAX_VALUE; i++)
+            sum += i;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        long start = System.nanoTime();
+        sum();
+        long end = System.nanoTime();
+        System.out.println((end - start) / 1_000_000. + " ms.");
+
+
+
+    }
+}
