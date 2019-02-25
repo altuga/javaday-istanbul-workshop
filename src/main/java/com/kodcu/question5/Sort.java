cc39a26ecb722848b013d13ad510a3df01d4a976
altuga@gmail.com <upux1234>
2019-02-25 13:35:27 +0300

    question 5 is added

---
+++ src/main/java/com/kodcu/question5/Sort.java
@@ -0,0 +1,12 @@
+package com.kodcu.question5;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Sort {
+
+    // Sort
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList(args);
+    }
+}
