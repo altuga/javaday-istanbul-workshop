404b95c1551f25af8fa4ba80e403acfd2245a30e
altuga@gmail.com <upux1234>
2019-02-27 13:30:31 +0300

    question 12 is added

---
+++ src/main/java/com/kodcu/question12/Card.java
@@ -0,0 +1,21 @@
+package com.kodcu.question12;
+
+public class Card {
+
+
+    enum Suit { CLUB, DIAMOND, HEART, SPADE }
+    enum Rank { ACE, DEUCE, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT,
+        NINE, TEN, JACK, QUEEN, KING }
+
+
+    private final Suit suit ;
+    private final Rank rank ;
+
+
+    public Card(Suit suit,  Rank rank ) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+
+}
---
+++ src/main/java/com/kodcu/question12/Symptom.java
@@ -0,0 +1,21 @@
+package com.kodcu.question12;
+
+
+import java.util.*;
+
+public class Symptom {
+
+    static Collection<Card.Suit> suits = Arrays.asList(Card.Suit.values());
+    static Collection<Card.Rank> ranks = Arrays.asList(Card.Rank.values());
+    static List<Card> deck = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+
+
+        for (Iterator<Card.Suit> i = suits.iterator(); i.hasNext(); )
+            for (Iterator<Card.Rank> j = ranks.iterator(); j.hasNext(); ) {
+                deck.add(new Card(i.next(), j.next()));
+            }
+    }
+}
